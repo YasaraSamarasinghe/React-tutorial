d663abfe28cd090d3a7daffa940085c22d934200
YasaraSamarasinghe <tharushiniyasara@gmail.com>
2020-04-08 13:49:43 +0530

    shouldComponentUpdate

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,7 +29,9 @@ class App extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
-    return true;
+    //return true;
+    return nextState.persons !== this.state.persons ||
+            nextState.showPerson !== this.state.showPerson;
   }
 
   componentWillUpdate(nextProps, nextState) {
@@ -104,6 +106,7 @@ class App extends Component {
     return (
       
       <div className={classes.App}>
+        <button onClick={() => {this.setState({showPerson: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
           showPerson={this.state.showPerson}
